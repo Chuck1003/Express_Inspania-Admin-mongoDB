90aacd2d326cfa01b7b4b87a03e1a1117682cb14
chenxiaofei <chenxiaofei@Ctrip.com>
2018-04-20 10:49:55 +0800

    tj

--- db.js
+++ db.js
@@ -1,6 +1,12 @@
-var settings = require("./settings");
+var settings = {
+    // "ip": "localhost",
+    "ip": "10.32.57.126",
+    "db": "test",
+    "host": 27071
+};
 var mongoose = require('mongoose');
 mongoose.connect("mongodb://"+settings.ip+"/"+settings.db);
+
 var db = mongoose.connection;
 
 module.exports={
--- settings.js
+++
@@ -1,6 +0,0 @@
-module.exports={
-    // "ip": "localhost",
-    "ip": "10.32.57.126",
-    "db": "test",
-    "host": 27071
-};
